26bbe1d90a27a4a60430f30ea8bd22895148e89b
Brandon Paulson <brandonpaulson28@gmail.com>
2023-08-06 20:24:22 -0500

    add all other exercise categories and make route generic using URL params

--- src/components/DialogueBox/DialogueBox.jsx
+++ src/components/DialogueBox/DialogueBox.jsx
@@ -15,18 +15,50 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import { Popover, Typography } from '@mui/material';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 
-const options = [
-  'Lats',
-  'Biceps',
-  'Back',
-  'Chest',
-  'Hamstrings',
-  'Calves',
-  'Quadriceps',
-  'Triceps'
-];
+// const options = [
+//   'Lats',
+//   'Biceps',
+//   'Back',
+//   'Chest',
+//   'Hamstrings',
+//   'Calves',
+//   'Quadriceps',
+//   'Triceps'
+// ];
 
- const bicepLink = <Link to="./biceps" underline="none"> Biceps </Link>
+// abdominals
+// abductors
+// adductors
+// biceps
+// calves
+// chest
+// forearms
+// glutes
+// hamstrings
+// lats
+// lower_back
+// middle_back
+// neck
+// quadriceps
+// traps
+// triceps
+
+const abdominalsLink = <Link to="./abdominals" underline="none"> Abdominals </Link>
+const abductorsLink = <Link to="./abductors" underline="none"> Abductors </Link>
+const adductorsLink = <Link to="./adductors" underline="none"> Adductors </Link>
+const bicepLink = <Link to="./biceps" underline="none"> Biceps </Link>
+const calvesLink = <Link to="./calves" underline="none"> Calves </Link>
+const chestLink = <Link to="./chest" underline="none"> Chest </Link>
+const forearmsLink = <Link to="./forearms" underline="none"> Forearms </Link>
+const glutesLink = <Link to="./glutes" underline="none"> Glutes </Link>
+const hamstringsLink = <Link to="./hamstrings" underline="none"> Hamstrings </Link>
+const latsLink = <Link to="./lats" underline="none"> Lats </Link>
+const lowerBackLink = <Link to="./lower_back" underline="none"> Lower-Back </Link>
+const middleBackLink = <Link to="./middle_back" underline="none"> Middle-Back </Link>
+const neckLink = <Link to="./neck" underline="none"> Neck </Link>
+const QuadricepsLink = <Link to="./quadriceps" underline="none"> Quadriceps </Link>
+const trapsLink = <Link to="./traps" underline="none"> Traps </Link>
+const tricepsLink = <Link to="./triceps" underline="none"> Triceps </Link>
 
 
 export default function BasicPopover() {
@@ -62,7 +94,10 @@ export default function BasicPopover() {
             horizontal: 'left',
           }}
         >
-          <Typography sx={{ p: 2 }}>{options[0]} {options[1]} {bicepLink}</Typography>
+          <Typography sx={{ p: 2 }}> {abdominalsLink}{abductorsLink}{adductorsLink} <br/>
+           {bicepLink} {calvesLink} {chestLink} <br/> {forearmsLink} {glutesLink} {hamstringsLink} <br/>
+           {latsLink} {lowerBackLink} {middleBackLink} <br/> {neckLink} {QuadricepsLink} {trapsLink} {tricepsLink}
+           </Typography>
         </Popover>
         <Button variant="contained" onClick={handleNotesClick}> Add Notes </Button>
       </div>
